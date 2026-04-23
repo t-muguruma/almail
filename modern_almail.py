40fc7372714455de2516f0eab286fe738516b67d
t-mugurum <roki.roki.1116@gmail.com>
2026-04-23 14:52:16 +0900

    バージョンを1.1.8に更新し、アップデートURLを修正。リポジトリのブランチが統一されてしまう現象を修正。

--- modern_almail.py
+++ modern_almail.py
@@ -60,8 +60,8 @@ class ModernALMail:
     RE_AUTH = {"spf": re.compile(r'spf=([a-z]+)', re.I), "dkim": re.compile(r'dkim=([a-z]+)', re.I), "dmarc": re.compile(r'dmarc=([a-z]+)', re.I), "header_from": re.compile(r'header\.from=([\w.-]+)', re.I)}
 
     APP_VERSION = "1.1.6" # 現在のプログラムのバージョン
-    UPDATE_URL = "https://raw.githubusercontent.com/t-muguruma/almail/main/version.json"
-    DOWNLOAD_BASE_URL = "https://raw.githubusercontent.com/sumeragi-sakura/Petal/main/"
+    UPDATE_URL = "https://raw.githubusercontent.com/t-muguruma/almail/master/version.json"
+    DOWNLOAD_BASE_URL = "https://raw.githubusercontent.com/t-muguruma/almail/main/"
 
     def __init__(self, root):
         self.root = root
